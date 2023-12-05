50967d127c13da74be67c27530d14765e4220af4
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2023-12-05 19:59:46 +0400

    Day4 Part1

---
+++ Day4/Part1/Program.cs
@@ -0,0 +1,17 @@
+﻿var cards = File.ReadAllLines("input.txt").Select(x => x[10..]).ToList();
+
+var totalPoints = cards.Select(CalcPointsForCard).Sum();
+
+Console.WriteLine(totalPoints);
+
+
+int CalcPointsForCard(string card)
+{
+    var matches = card
+        .Split(new char[] { '|', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .GroupBy(n => n)
+        .Count(g => g.Count() >= 2);
+
+    return (int)Math.Pow(2, matches - 1);
+}
