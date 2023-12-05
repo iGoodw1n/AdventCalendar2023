941f9f85212bd71adf99610a0b015b160279eed1
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2023-12-05 23:07:06 +0400

    Day4 Part2

--- Day4/Part2/Program.cs
+++ Day4/Part2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var cards = File.ReadAllLines("input.txt").Select(x => x[10..]).ToList();
+
+
+var copies = Enumerable.Repeat(1, cards.Count).ToList();
+for (int i = 0; i < cards.Count; i++)
+{
+    var matches = CalcPointsForCard(cards[i]);
+   
+    for (int j = i + 1; j <= i + matches && j < cards.Count; j++)
+    {
+        copies[j] += copies[i];
+    }
+
+}
+
+foreach (var item in copies)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine(copies.Sum(x => x));
+
+
+
+int CalcPointsForCard(string card)
+{
+    var matches = card
+        .Split(new char[] { '|', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .GroupBy(n => n)
+        .Count(g => g.Count() >= 2);
+
+    return matches;
+}
