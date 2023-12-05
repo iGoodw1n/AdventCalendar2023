cb0ba9aa2f5a30bd40bb9cf107962fd508ab9c2f
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2023-12-06 00:30:01 +0400

    Day5 Part1

---
+++ Day5/Part1/Program.cs
@@ -0,0 +1,28 @@
+﻿var data = File.ReadAllText("input.txt").Split("\r\n\r\n");
+var seeds = data[0][7..].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+
+foreach (var mapData in data.Skip(1))
+{
+    var map = mapData.Split("\r\n").Skip(1).Select(ConvertToTuple).ToList();
+
+    for (int i = 0; i < seeds.Count; i++)
+    {
+        seeds[i] = Map(seeds[i], map);
+    }
+}
+
+Console.WriteLine(seeds.Min());
+
+static (long SourceStart, long Range, long DestStart) ConvertToTuple(string line)
+{
+    var data = line.Split(' ').Select(long.Parse).ToList();
+    return (data[1], data[2], data[0]);
+}
+
+
+long Map(long value, List<(long SourceStart, long Range, long DestStart)> list)
+{
+    var dest = list.Find(x => value >= x.SourceStart && value <= x.SourceStart + x.Range);
+
+    return dest == default ? value : dest.DestStart + value - dest.SourceStart;
+}
