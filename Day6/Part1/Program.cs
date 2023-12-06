d86c41018917e891d49ed0410d42e652ae5875b9
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2023-12-06 18:38:19 +0400

    Day6 Part1

---
+++ Day6/Part1/Program.cs
@@ -0,0 +1,47 @@
+﻿var data = File.ReadAllLines("input.txt");
+
+var times = ParseData(data[0]);
+var distances = ParseData(data[1]);
+
+var allPossibleWays = 1;
+
+for (int i = 0; i < times.Count; i++)
+{
+    allPossibleWays *= GetAllPossibleWins(times[i], distances[i]);
+}
+
+Console.WriteLine(allPossibleWays);
+
+List<int> ParseData(string data)
+{
+    return data.Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList();
+}
+
+int GetAllPossibleWins(int time, int distance)
+{
+    var min = 1;
+    for (; min <= time; min++)
+    {
+        if (CalcDistance(min) > distance)
+        {
+            break;
+        }
+    }
+
+    var max = time - 1;
+
+    for (; max > min; max--)
+    {
+        if (CalcDistance(max) > distance)
+        {
+            break;
+        }
+    }
+
+    return max - min + 1;
+
+    int CalcDistance(int seconds)
+    {
+        return (time - seconds) * seconds;
+    }
+}
